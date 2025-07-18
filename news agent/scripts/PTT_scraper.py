f7a519b6d5977c7656e113d9dad1c9aa9bfe887d
potentialfirehazard <madelynsk7@gmail.com>
2025-07-17 18:24:20 -0700

    first commit

---
+++ news agent/scripts/PTT_scraper.py
@@ -0,0 +1,130 @@
+'''This program is an HTML web scraper that obtains title, source, body, url, timestamp, and keywords from articles from
+the PTT Stock Board. THe data obtained is uploaded to MongoDB. This program is specific to the PTT Stock Board, and cannot
+be used for other websites due to different HTML structures.'''
+
+from bs4 import BeautifulSoup # for HTML parsing
+import bs4 # for HTML parsing
+import requests # to get the HTML of websites
+import csv # to parse the keyword filter set
+from pymongo import MongoClient # to upload to MongoDB
+
+# gets the BeautifulSoup object of the webpage for scraping
+def get_article(url):
+    response = requests.get(url)
+    html_content = response.text
+
+    return BeautifulSoup(html_content, "lxml")
+
+# gets the body text of a PTT Stock Board Post
+def get_body_text(soup):
+
+    article = soup.find_all(class_ = "article-metaline") # uses article-metaline tags as starting point for body text
+    tag = article[len(article) - 1] # finds the last article-metaline tag
+
+    text = ""
+    while True:
+        # breaks from the loop if the end of the page is reached
+        if tag == None or tag.next_sibling == None:
+            break
+
+        # checks if the next sibling is a tag
+        if isinstance(tag, bs4.Tag):
+
+            # checks if the next tag has a class
+            if tag.get("class") != None:
+
+                # ends the loop if the article has ended and the next tag is for comments
+                if tag.get("class")[0] == "push":
+                    break
+        
+        # adds the current element to text if it is not a tag object
+        else:
+            text = text + str(tag)
+        
+        # sets tag to the next item in the webpage
+        tag = tag.next_sibling
+
+    # removing substrings used for formatting on the website for better legibility in the BSON document
+    text = text.replace("\n", "")
+    text = text.replace("  ", "")
+    text = text.replace("===", "")
+    text = text.replace("---", "")
+
+    return(text)
+
+# fetches a specified number of articles from the PTT Stock Board
+def fetch(number):
+    
+    client = MongoClient("mongodb+srv://madelynsk7:vy97caShIMZ2otO6@testcluster.aosckrl.mongodb.net/") # replace string with wanted connection string
+    database = client["news_info"] # replace string with wanted database
+    counter = 0 # counts the number of articles fetched
+    url = "https://www.ptt.cc/bbs/stock/index.html"
+    cont = True
+
+    # opens keyword filter set
+    with open("news agent\data\keyword_filter_set_zh.csv", mode = "r", encoding = "utf-8", newline = "") as file:
+
+        # loops until the number of articles needed is met
+        while cont:
+
+            # finds all articles on the homepage
+            content = get_article(url)
+            articles = content.find_all(class_ = "title", limit = 350)
+
+            # loops for each article on the page
+            for i in articles:
+
+                # finds the tag holding the link and title of each article
+                info = i.find("a")
+                if info != None:
+                    # parses the article linked
+                    article_link = "https://www.ptt.cc" + info.get("href")
+                    article = get_article(article_link)
+                    
+                    # finds where the body text begins
+                    for i in article.find_all(class_ = "article-metaline"):
+                        if i.find(class_ = "article-meta-tag").get_text() == "時間":
+                            time = i.find(class_ = "article-meta-value").get_text()
+                    
+                    # finds body text
+                    text = get_body_text(article)
+                    
+                    article_keywords = []
+                    
+                    # loops through the keyword filter list
+                    csv_reader = csv.reader(file)
+                    for row in csv_reader:
+                        if row: # check if the row isn't empty
+                            keyword = row[0]
+                            if keyword in info.get_text() or keyword in text: # adds the keyword to the list if it is found
+                                article_keywords.append(keyword)
+                    
+                    # creates the file to be stored
+                    data = {
+                        "title" : info.get_text(),
+                        "source" : "PTT Stock Board",
+                        "body" : text,
+                        "url" : article_link,
+                        "timestamp" : time,
+                        "keywords" : article_keywords
+                    }
+
+                    # adds the file to the database
+                    database.article_info.insert_one(data)
+                    counter += 1
+                    
+                    # stops fetching if the number of articles needed is reached
+                    if counter >= number:
+                        cont = False
+                        break
+
+            # finds all the buttons for navigating the page
+            buttons = get_article(url).find_all(class_ = "btn wide")
+
+            # finds the button to go to the next page and sets the link for parsing to the next page
+            for i in buttons:
+                    if i.get_text() == "‹ 上頁":
+                        url = "https://www.ptt.cc" + i.get("href")
+    
+    # closes MongoClient
+    client.close()
---
+++ news agent/scripts/main.py
@@ -0,0 +1,106 @@
+'''This program will fetch title, source, body, url, timestamp, and keywords from articles, fetching 350 at a time, 3 times daily.
+Data is obtained primarily through RSS parsing, and then web scraping the PTT stock board with HTML. The data obtained is uploaded
+to MongoDB.'''
+
+import feedparser # for RSS parsing
+from pymongo import MongoClient # for uploading data to MongoDB
+import schedule # for scheduling fetches 3 times daily
+import time # to sleep the program when not running
+import csv # for parsing the keyword filter set
+import PTT_scraper # getting the script for the HTML scraper for the PTT stock board
+
+# list of urls and source for higher quality websites, indexes of url and source correspond
+high_priority_url_list = ["https://news.cnyes.com/rss/v1/news/category/tw_stock", "https://news.cnyes.com/rss/v1/news/category/all", "https://news.cnyes.com/rss/v1/news/category/headline", "https://www.moneydj.com/kmdj/RssCenter.aspx?svc=NW&fno=1&arg=X0000000", "https://tw.stock.yahoo.com/rss?category=tw-market"]
+high_priority_source_list = ["鉅亨網 (Anue)", "鉅亨網 (Anue)", "鉅亨網 (Anue)", "MoneyDJ 理財網", "Yahoo 奇摩股市"]
+
+# list of urls and source for lower quality websites, indexes of url and source correspond
+lower_priority_url_list = ["https://cmsapi.businessweekly.com.tw/?CategoryId=efd99109-9e15-422e-97f0-078b21322450&TemplateId=8E19CF43-50E5-4093-B72D-70A912962D55", "https://techorange.com/feed/", "https://www.inside.com.tw/feed/rss", "https://feeds.feedburner.com/rsscna/finance"]
+lower_priority_source_list = ["商業週刊", "TechOrange 科技報橘", "Inside (科技媒體)", "中央社財經 (CNA)"]
+
+counter = 0 # counts the number of articles fetched
+
+# fetches articles from one source
+def fetch(url, title):
+    # connects to the MongoDB database
+    client = MongoClient("mongodb+srv://madelynsk7:vy97caShIMZ2otO6@testcluster.aosckrl.mongodb.net/") # replace string with the wanted connection string
+    database = client["news_info"] # replace string with the name of the wanted database
+
+    NewsFeed = feedparser.parse(url) # parses the RSS of the url
+
+    # opens the keyword filter file
+    with open("news agent\data\keyword_filter_set_zh.csv", mode = "r", encoding = "utf-8", newline = "") as file:
+
+        # loops through each entry in the RSS feed
+        for i in range(len(NewsFeed.entries)):
+            global counter
+
+            # stops fetching data from articles if the 350 limit is reached
+            if counter >= 350:
+                break
+            
+            article_keywords = []
+            
+            # loops through the keyword filter set
+            csv_reader = csv.reader(file)
+            for row in csv_reader:
+                if row: #check if the row isn't empty
+                    keyword = row[0]
+
+                    # adds the article keyword if one is found
+                    if keyword in NewsFeed.entries[i].title or keyword in NewsFeed.entries[i].description:
+                        article_keywords.append(keyword)
+
+            # creates the file to be stored
+            article = {
+                "title" : NewsFeed.entries[i].title,
+                "source" : title,
+                "body" : NewsFeed.entries[i].description,
+                "url" : NewsFeed.entries[i].link,
+                "timestamp" : NewsFeed.entries[i].published,
+                "keywords" : article_keywords
+            }
+
+            # stores the article
+            database.article_info.insert_one(article) # replace "article_info" with the name of the desired collection
+            counter += 1
+
+    client.close() # closes MongoClient
+
+# fetches 350 articles
+def daily_fetch():
+    global counter
+
+    # fetches articles from high priority websites w/ the 350 article limit
+    index = 0
+    while counter <= 350:
+        # breaks out of the loop if every website has been visited
+        if index >= len(high_priority_url_list):
+            break
+        fetch(high_priority_url_list[index], high_priority_source_list[index])
+        index += 1
+    
+    # fetches articles from lower priority websites w/ the 350 article limit
+    index = 0
+    while counter <= 350:
+        if index >= len(lower_priority_url_list):
+            break
+        fetch(lower_priority_url_list[index], lower_priority_source_list[index])
+        index += 1
+    
+    # fetches the rest of the 350 articles from the PTT stock board
+    if counter < 350:
+        num = 350 - counter
+        PTT_scraper.fetch(num)
+
+    print(counter)
+
+daily_fetch()
+# schedules the daily fetch for the three times each day, in Taiwan's time zone
+schedule.every().day.at("07:30", "Asia/Hong_Kong").do(daily_fetch)
+schedule.every().day.at("13:30", "Asia/Hong_Kong").do(daily_fetch)
+schedule.every().day.at("18:00", "Asia/Hong_Kong").do(daily_fetch)
+
+# pauses the program while waiting for the scheduled fetches
+while True:
+    schedule.run_pending()
+    time.sleep(1)
